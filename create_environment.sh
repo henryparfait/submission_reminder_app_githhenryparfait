f5733fcd4b945ab98d74dfa6e235f43ca85243ed
henryparfait <p.uhiriwe@alustudent.com>
2024-10-02 17:35:48 +0200

    Added to reminder.sh

--- create_environment.sh
+++ create_environment.sh
@@ -24,3 +24,21 @@ touch config/config.env
 # Create startup.sh in the main directory
 touch startup.sh
 
+cat <<EOL > ~/shell_scripting_lab/submission_reminder_app/app/reminder.sh
+#!/bin/bash
+
+# Source environment variables and helper functions
+source ./config/config.env
+source ./modules/functions.sh
+
+# Path to the submissions file
+submissions_file="./assets/submissions.txt"
+
+# Print remaining time and run the reminder function
+echo "Assignment: $ASSIGNMENT"
+echo "Days remaining to submit: $DAYS_REMAINING days"
+echo "--------------------------------------------"
+
+check_submissions $submissions_file
+EOL
+chmod u+x ~/shell_scripting_lab/submission_reminder_app/app/reminder.sh

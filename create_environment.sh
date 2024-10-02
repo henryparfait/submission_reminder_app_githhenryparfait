989610ab724cd2d56561973228d93e726b4bf0c0
henryparfait <p.uhiriwe@alustudent.com>
2024-10-02 16:36:08 +0200

    New shell script

--- create_environment.sh
+++ create_environment.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+# Create main directory
+mkdir -p submission_reminder_app
+
+# Navigate into it
+cd submission_reminder_app
+
+# Create subdirectories
+mkdir -p app modules assets config
+
+# Create files inside 'app' directory
+touch app/reminder.sh
+
+# Create files inside 'modules' directory
+touch modules/functions.sh
+
+# Create files inside 'assets' directory
+touch assets/submissions.txt
+
+# Create files inside 'config' directory
+touch config/config.env
+
+# Create startup.sh in the main directory
+touch startup.sh
+
